0cdd36314024684434596231eaf91792f451561b
toBeOfUse <mitchjacov@gmail.com>
2022-11-26 10:49:53 -0500

    argparse hello world

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("hello world 🥺");
+import { ArgumentParser } from "argparse";
+
+const parser = new ArgumentParser({
+    description: "Bundle README files from different directories into a single Markdown file."
+});
+
+parser.add_argument("roots", {
+    metavar: "root_directory",
+    type: "str",
+    nargs: "+"
+});
+
+console.dir(parser.parse_args());
